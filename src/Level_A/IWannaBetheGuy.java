ccc7834c968dff5a51fd30f27c3405bb2747f9d6
eman amassi <wamda1998a@gmail.com>
2020-05-20 01:56:29 +0300

    AddNewQuestion

---
+++ src/Level_A/IWannaBetheGuy.java
@@ -0,0 +1,38 @@
+package Level_A;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class IWannaBetheGuy {
+
+    private static Scanner scan= new Scanner(System.in);
+
+    public static void main(String [] args){
+        int n= scan.nextInt();
+
+        HashSet<Integer> levels= new HashSet<>();
+
+        int p= scan.nextInt();
+
+        for(int i= 0; i < p; i++){
+            levels.add(scan.nextInt());
+        }
+
+        int q= scan.nextInt();
+
+        for(int i= 0; i < q; i++){
+            levels.add(scan.nextInt());
+        }
+
+        //System.out.println(xLittle+" " + yLittle+ " " +levels);
+
+        for(int i= 1; i <= n; i++){
+            if(!levels.contains(i)){
+                System.out.println( "Oh, my keyboard!");
+                return;
+            }
+        }
+
+        System.out.println("I become the guy.");
+    }
+}
