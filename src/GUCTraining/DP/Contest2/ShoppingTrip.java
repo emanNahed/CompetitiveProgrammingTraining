c8520712a34e9fba237c29ee57413cf34adccca8
Eman Amassi <wamda1998a@gmail.com>
2020-07-28 12:31:22 +0300

    u problems

--- src/GUCTraining/DP/Contest2/ShoppingTrip.java
+++ src/GUCTraining/DP/Contest2/ShoppingTrip.java
@@ -5,23 +5,25 @@ import Reusable_Peices.SScanner;
 import java.awt.desktop.SystemSleepEvent;
 import java.io.IOException;
 import java.util.Arrays;
-
 public class ShoppingTrip {
 
     static SScanner scan= new SScanner(System.in);
     static double [][] arr;
-    static double [] save;
+    static double [][] save;
+    static double [][] dp;
+    static int saves;
     public static void main(String [] args) throws IOException {
         int t= scan.nextInt();
 
         while (t-- != 0){
-            //scan.nextLine();
+            scan.nextLine();
 
             int stores= scan.nextInt();
             int roads= scan.nextInt();
 
-             arr= new double[stores + 1][stores + 1]; // + 1 for home
+            arr= new double[stores + 1][stores + 1]; // + 1 for home
             for(double[] doubles: arr) Arrays.fill(doubles, Double.MAX_VALUE);
+            for(int i= 0; i< stores; i++) arr[i][i]= 0;
 
             for(int i= 0; i< roads; i++){
                 int a= scan.nextInt();
@@ -29,43 +31,58 @@ public class ShoppingTrip {
 
                 double c= scan.nextDouble();
 
-                arr[a][b]= c;
-                arr[b][a] = c;
+                arr[a][b]= Math.min(arr[a][b], c);
+                arr[b][a] = arr[a][b];
             }
 
-            int saves= scan.nextInt();
-            save= new double[stores + 1];
-            for(int i= 0; i< saves; i++) {
+            saves= scan.nextInt();
+            save= new double[saves + 1][2];
+            save[0][0] = 0; save[0][1] = 0;
+            for(int i= 1; i< saves + 1; i++) {
                 int store= scan.nextInt();
-                save[store] = scan.nextDouble();
+                save[i][0] = store;
+                save[i][1]= scan.nextDouble();
             }
 
 
             //apply floyd algorithm (my first time :")) )
-            for(int intermediate= 0; intermediate < stores; intermediate++)
+            for(int intermediate= 0; intermediate < stores + 1; intermediate++)
 
-                for(int src= 0; src < stores; src++)
+                for(int src= 0; src < stores + 1; src++)
 
-                    for(int dst= 0; dst< stores; dst++)
+                    for(int dst= 0; dst< stores +1; dst++)
                         if(arr[src][intermediate] + arr[intermediate][dst] < arr[src][dst])
                             arr[src][dst]= arr[src][intermediate] + arr[intermediate][dst];
 
 
+            dp= new double[stores + 1][(1 << save.length)];
+            for(double [] doubles: dp) Arrays.fill(doubles, -1.0);
 
-            System.out.println(maxSave(0, 1));
+            double result= maxSave(0, 1);
+            if(result > 0.0){
+                System.out.print("Daniel can save $");
+                System.out.printf("%.2f", result);
+                System.out.println();
+            }
+            else
+                System.out.println("Don't leave the house");
         }
     }
 
     private static double maxSave(int current, int mask) {
-        if(mask == (1 << arr.length) - 1) return - arr[current][0];
+        if(mask == (1 << save.length) - 1) return - arr[(int)save[current][0]][0];
 
-        double max= 0;
-        for(int i= 0; i< arr.length; i++)
+         if(dp[current][mask] != -1) {
+             return dp[current][mask];
+         }
+
+
+        double max= -arr[(int)save[current][0]][0]; //which is better return home from this store or complete other stores?
+        for(int i= 0; i< save.length; i++)
             if((mask & (1 << i)) == 0)
-                max= Math.max(max,  (save[i] - arr[current][i]) + maxSave(i, mask | (1 << i)));
+                max= Math.max(max,  (save[i][1] - arr[(int) save[current][0]][(int) save[i][0]]) + maxSave(i, mask | (1 << i)));
 
-
-
-        return max;
+        return dp[current][mask]= max;
     }
+
 }
